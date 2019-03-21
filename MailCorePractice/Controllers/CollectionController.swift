3fc13aa0b1aea9c72cd65f45039efac97306a92e
Koh Jia Rong <a0097723@gmail.com>
2019-03-21 17:41:03 +0800

    commit to dev

---
+++ MailCorePractice/Controllers/CollectionController.swift
@@ -0,0 +1,9 @@
+//
+//  CollectionController.swift
+//  MailCorePractice
+//
+//  Created by Koh Jia Rong on 2019/3/21.
+//  Copyright © 2019 Koh Jia Rong. All rights reserved.
+//
+
+import Foundation
---
+++ MailCorePractice/Models/Utilities.swift
@@ -0,0 +1,9 @@
+//
+//  Utilities.swift
+//  MailCorePractice
+//
+//  Created by Koh Jia Rong on 2019/2/22.
+//  Copyright © 2019 Koh Jia Rong. All rights reserved.
+//
+
+import Foundation
